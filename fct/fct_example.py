e2925d029378fed58a9538ae821355d0e3971abb
tilmaerts <95282593+tilmaerts@users.noreply.github.com>
2024-07-04 13:42:33 +0200

    get examples to run

--- fct/fct_example.py
+++ fct/fct_example.py
@@ -5,8 +5,13 @@ import importlib.util
 
 def main():
     destination = os.path.join(os.getcwd(), "examples")
+    destinationr = os.path.join(os.getcwd(), "examples", "fct_resources")
+
     module_path = os.path.abspath(
         os.path.join(os.path.dirname(__file__), "..", "fct_examples")
+    )
+    module_path_resources = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "fct_resources")
     )
 
     # print(module_path)
@@ -19,4 +24,5 @@ def main():
     # print(spec)
 
     shutil.copytree(module_path, destination)
+    shutil.copytree(module_path_resources, destinationr)
     print(f"Examples directory created at {destination}")
